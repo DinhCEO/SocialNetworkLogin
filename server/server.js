6c8019f9ee344df00e0d914473914d9fa048583c
obama <pvd02091992@gmail.com>
2018-01-02 17:03:27 +0700

    add template view

--- server/server.js
+++ server/server.js
@@ -2,13 +2,20 @@ require('dotenv').config();
 const express    = require('express');
 const cors       = require('cors');
 const bodyParser = require('body-parser');
+var nunjucks     = require('nunjucks');
 
 const app  = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
-
+nunjucks.configure('pages', {
+    express   : app,
+    autoescape: true
+});
+app.get('/', function (req, res) {
+    res.render('index.html',{data:'dinhceo'});
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening port ${PORT}`);
