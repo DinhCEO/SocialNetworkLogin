db87a73e7c630cd63a27ed67a230c76c9b0418c0
obama <pvd02091992@gmail.com>
2018-01-03 15:30:43 +0700

    login google

--- server/index.js
+++ server/index.js
@@ -24,27 +24,60 @@ app.get('/login', (req, res) => {
 });
 app.get('/getProfile', (req, res) => {
     let accessToken = req.query['accessToken'];
-    if (!accessToken) {
+    let typeLogin   = req.query['type'];
+    if (!accessToken || !typeLogin) {
         return res.status(400).json({msg: 'Login failed!'})
     }
-    let options = {
-        uri    : 'https://graph.facebook.com/me',
-        qs     : {
-            access_token: accessToken,
-            fields      : 'last_name,name,email,gender,first_name,birthday,link,website,picture'
-        },
-        headers: {
-            'User-Agent': 'Request-Promise'
-        },
-        json   : true
-    };
-    request(options)
-        .then(user => {
-            res.render('profile.html', {user: user});
-        })
-        .catch(() => {
-            res.redirect('/login')
-        });
+    let userFormat = {};
+    if (typeLogin === 'facebook') {
+        let options = {
+            uri    : 'https://graph.facebook.com/me',
+            qs     : {
+                access_token: accessToken,
+                fields      : 'last_name,name,email,gender,first_name,birthday,link,website,picture'
+            },
+            headers: {
+                'User-Agent': 'Authenticate-Facebook'
+            },
+            json   : true
+        };
+        request(options)
+            .then(user => {
+                userFormat.email  = user.email;
+                userFormat.avatar = user.picture.data.url;
+                userFormat.name   = user.name;
+                userFormat.link   = user.link;
+                userFormat.size   = 50;
+                res.render('profile.html', {user: userFormat});
+            })
+            .catch(() => {
+                res.redirect('/login')
+            });
+    } else if (typeLogin === 'google') {
+        let optionsGoogle = {
+            uri    : 'https://www.googleapis.com/plus/v1/people/me',
+            qs     : {
+                access_token: accessToken,
+            },
+            headers: {
+                'User-Agent': 'Authenticate-Google'
+            },
+            json   : true
+        };
+        request(optionsGoogle)
+            .then(user => {
+                userFormat.email  = user.emails[0].value;
+                userFormat.avatar = user.image.url;
+                userFormat.name   = user.displayName;
+                userFormat.link   = user.url;
+                userFormat.size   = 50;
+                res.render('profile.html', {user: userFormat});
+            })
+            .catch((error) => {
+                console.log(error);
+                res.redirect('/login')
+            });
+    }
 });
 app.listen(PORT, () => {
     console.log(`Server listening port ${PORT}`);
